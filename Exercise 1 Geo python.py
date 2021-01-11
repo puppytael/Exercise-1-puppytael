a80a6876ea8c9161c438067b095e62d5b9ffe43c
puppytael <odhambrian@gmail.com>
2021-01-11 11:23:24 +0300

    added exercise 2 geopython

---
+++ Exercise 1 Geo python.py
@@ -0,0 +1,23 @@
+# Problem 1 Defening some variables
+ice_cream_rating = 6
+sleeping_rating = 5
+print(ice_cream_rating)
+print(sleeping_rating)
+print()
+# Problem 2 Reading in variable values
+first_name = input("What is your first name")
+last_name = input("What is your last name")
+my_name = first_name + last_name
+print(my_name)
+# Problem 3 bit of math
+happiness_rating = (ice_cream_rating + sleeping_rating) / 2
+print(happiness_rating)
+# Problem 4 data types
+print(type(happiness_rating))
+print(type(sleeping_rating))
+print(type(first_name))
+# Problem 5
+print("My name is", my_name, "and i give ice cream a rating of", ice_cream_rating, "out of 10 ")
+print("My name is", my_name, "and i give ice cream a rating of", sleeping_rating, "out of 10 ")
+print("My name is", my_name, "and based on the factors above my hapiness rating is", happiness_rating, "out of 10")
+print()
